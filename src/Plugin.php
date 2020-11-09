30d5bd3d5b956258275dd581dd96200fed6f8153
Jonas Eberle <flightvision@googlemail.com>
2020-11-09 22:01:39 +0100

    [TASK] set plugin package to typo3-contentblocks/composer-plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package typo3-contentblocks/content-blocks-composer-plugin.
+ * This file is part of the package typo3-contentblocks/composer-plugin.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
